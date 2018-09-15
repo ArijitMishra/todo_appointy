e1353a86be4078f47de3b630e0aa21080547e529
Arijit Kumar Mishra <arijitmishra143@gmail.com>
2018-09-15 20:54:35 +0530

    Update main.dart

--- main.dart
+++ main.dart
@@ -1,13 +1,11 @@
-// Import MaterialApp and other widgets which we can use to quickly create a material app
+
 import 'package:flutter/material.dart';
 
-// Code written in Dart starts exectuting from the main function. runApp is part of
-// Flutter, and requires the component which will be our app's container. In Flutter,
-// every component is known as a "widget".
+
 void main() => runApp(new TodoApp());
 //bool _value = false;
 
-// Every component in Flutter is a widget, even the whole app itself
+
 class TodoApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -30,8 +28,7 @@ class TodoListState extends State<TodoList> {
   void _addTodoItem(String task) {
     // Only add the task if the user actually entered something
     if(task.length > 0) {
-      // Putting our code inside "setState" tells the app that our state has changed, and
-      // it will automatically re-render the list
+     
       setState(() => _todoItems.add(task));
     }
   }
@@ -48,9 +45,7 @@ class TodoListState extends State<TodoList> {
               title: new Text('Mark "${_todoItems[index]}" as done?'),
               actions: <Widget>[
                 new FlatButton(
-                    child: new Text('CANCEL'),
-                    // The alert is actually part of the navigation stack, so to close it, we
-                    // need to pop it.
+                    child: new Text('CANCEL'),                    
                     onPressed: () => Navigator.of(context).pop()
                 ),
                 new FlatButton(
@@ -73,10 +68,7 @@ class TodoListState extends State<TodoList> {
   // Build the whole list of todo items
   Widget _buildTodoList() {
     return new ListView.builder(
-      itemBuilder: (context, index) {
-        // itemBuilder will be automatically be called as many times as it takes for the
-        // list to fill up its available space, which is most likely more than the
-        // number of todo items we have. So, we need to check the index is OK.
+      itemBuilder: (context, index) {        
         if(index < _todoItems.length) {
           return _buildTodoItem(_todoItems[index], index);
         }
@@ -86,10 +78,7 @@ class TodoListState extends State<TodoList> {
 
   Widget _buildCompleteList() {
     return new ListView.builder(
-      itemBuilder: (context, index) {
-        // itemBuilder will be automatically be called as many times as it takes for the
-        // list to fill up its available space, which is most likely more than the
-        // number of todo items we have. So, we need to check the index is OK.
+      itemBuilder: (context, index)
         if(index < _completeItems.length) {
           return _buildTodoItem(_completeItems[index], index);
         }
